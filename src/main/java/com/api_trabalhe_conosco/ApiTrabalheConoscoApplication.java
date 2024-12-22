27ee675d0e4f38d11390a473b6d2cd4de3943aa1
hitalo.siriano <hitalo.siriano@gmail.com>
2024-12-22 11:01:44 -0300

    feat: projeto criado.

---
+++ src/main/java/com/api_trabalhe_conosco/ApiTrabalheConoscoApplication.java
@@ -0,0 +1,13 @@
+package com.api_trabalhe_conosco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTrabalheConoscoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTrabalheConoscoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api_trabalhe_conosco/ApiTrabalheConoscoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api_trabalhe_conosco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTrabalheConoscoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
